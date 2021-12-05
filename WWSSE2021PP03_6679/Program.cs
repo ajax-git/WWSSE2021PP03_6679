81baab984ce9880aaf059638ded95beeabc82274
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-05 19:56:34 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP03_6679/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+    Napisz program, który pyta użytkownika o podanie liczby całkowitych a w wyniku wypisuje komunikat o ilości cyfr.
+*/
+
+Console.WriteLine("Podaj liczbę całkowitą: ");
+
+string str = Console.ReadLine();
+
+if(str.Length >= 2) Console.WriteLine("Liczba {0} posiada {1} cyfry.", str, str.Length);
+    else Console.WriteLine("Liczba {0} posiada {1} cyfrę.", str, str.Length);
